65d80e4e653e9341745823335187fbb0aa60ae06
infabls <infabls.com@gmail.com>
2020-11-24 16:22:18 +0600

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -182,19 +182,22 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'admin'], f
  * 
  * @return Float Distance in Kilometers.
  */
-function getDistanceBetweenPointsNew($latitude1, $longitude1, $latitude2, $longitude2, $unit = 'Mi') {
-    $theta = $longitude1 - $longitude2;
-    $distance = sin(deg2rad($latitude1)) * sin(deg2rad($latitude2)) + cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * cos(deg2rad($theta));
 
-    $distance = acos($distance); 
-    $distance = rad2deg($distance); 
-    $distance = $distance * 60 * 1.1515;
+if (!function_exists('getDistanceBetweenPointsNew')) {
+	function getDistanceBetweenPointsNew($latitude1, $longitude1, $latitude2, $longitude2, $unit = 'Mi') {
+	    $theta = $longitude1 - $longitude2;
+	    $distance = sin(deg2rad($latitude1)) * sin(deg2rad($latitude2)) + cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * cos(deg2rad($theta));
 
-    switch($unit) 
-    { 
-        case 'Mi': break;
-        case 'Km' : $distance = $distance * 1.609344; 
-    }
+	    $distance = acos($distance); 
+	    $distance = rad2deg($distance); 
+	    $distance = $distance * 60 * 1.1515;
 
-    return (round($distance,3)); 
+	    switch($unit) 
+	    { 
+	        case 'Mi': break;
+	        case 'Km' : $distance = $distance * 1.609344; 
+	    }
+
+	    return (round($distance,3)); 
+	}
 }
